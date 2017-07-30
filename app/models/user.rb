1bb27436bd763336e25b8f37b8e849f34aa6430f
Utku Kaynar <utku@code2.co>
2017-07-30 10:13:33 +0300

    Add email format and password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ApplicationRecord
   has_one :profile
   accepts_nested_attributes_for :profile
-
   validates :email, :password, :role, presence: true
+  validates :email, format: { with: /([\.\w\-]+@\w+\.\w{2,3}(?:\.?\.?\w{2,3})?)/ }
+  validates :password, length: { minimum: 8, maximum: 30 }
 end
