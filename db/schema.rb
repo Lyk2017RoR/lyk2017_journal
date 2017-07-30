81ad7dd90de67751fa86ca6f341e24fdfbd96e14
Utku Kaynar <utku@code2.co>
2017-07-30 10:32:17 +0300

    Add join table for posts and tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730064608) do
+ActiveRecord::Schema.define(version: 20170730071920) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20170730064608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
   end
 
   create_table "profiles", force: :cascade do |t|
