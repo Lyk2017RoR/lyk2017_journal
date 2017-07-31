d8fba8cda3a7f36d8ad15c7b3284c1d95ff866ed
Utku Kaynar <utku@code2.co>
2017-07-31 19:26:10 +0300

    Add comment test

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Comments", type: :feature do
   context "Registered User" do
+    scenario 'can view comments of a post'
     scenario 'can comment on a post'
     scenario 'can delete his/her comment'
     scenario 'can edit his comment'
