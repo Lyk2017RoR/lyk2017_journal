8664fc827b5cc1af96da9f3eac8714985af709f2
Utku Kaynar <utku@code2.co>
2017-07-31 15:50:29 +0300

    Complete tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -32,4 +32,28 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context 'with valid params' do
+      let(:valid_params) { { email: "utkukaynar@gmail.com", password: "asdqwe12", password_confirmation: 'asdqwe12' } }
+      it 'creates a new user' do
+        expect{ post :create, user: valid_params }.to change(User, :count).by 1
+      end
+      it 'redirects to user page' do
+        post :create, user: valid_params
+        expect(response).to redirect_to(user_path(User.last))
+      end
+    end
+
+    context 'with invalid params' do
+      let(:invalid_params) { { email: "sadasdasdasd", password: "asdqwe12", password_confirmation: '12345677' } }
+      it 'doesn\'t create a new user' do
+        expect{ post :create, user: invalid_params }.not_to change(User, :count)
+      end
+      it 'redirects to new user page' do
+        post :create, user: invalid_params
+        expect(response).to redirect_to new_user_path
+      end
+    end
+  end
+
 end
