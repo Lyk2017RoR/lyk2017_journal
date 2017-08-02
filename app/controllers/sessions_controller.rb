977028be8b4bc8a7a1ad6fd35d6a23f0674b99d4
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-02 19:33:15 +0300

    Session oluşturulurken kullanılan parametreler user array'inden alınıyor ve flash mesajlar her actionda set ediliyor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
       # Find user with the email and authenticate with password.
       session[:user_id] = user.id
+      flash[:info] = 'Başarıyla Giriş Yaptınız'
       redirect_to user_path(user)
     else
+      flash[:error] = 'Email veya Parolanız Yanlış'
       redirect_to new_session_path
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:info] = 'Başarıyla Çıkış Yaptınız'
     redirect_to root_path
   end
 end
