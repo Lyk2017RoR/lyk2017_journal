d7f6431434455603f1c8cdc00505364b8f34e651
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-03 01:11:31 +0300

    User modelinde hizalama ve validasyon düzenlemeleri yapıldı

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,20 +9,30 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
-
 class User < ApplicationRecord
   has_secure_password
-  has_one :profile, inverse_of: :user
-  has_many :posts
-  has_many :likes
-  has_many :liked_posts, through: :likes, source: :post
-  has_many :comments
+
+  has_one :profile, inverse_of: :user, dependent: :destroy
+
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  enum role: %i[regular editor admin]
+
+  validates :email,         presence: true,
+                            format: {
+                              with: /([\w\.\-]+@\w+\.\w{2,3}(?:\.?\.?\w{2,3})?)/
+                            },
+                            uniqueness: true
+
+  validates :password,      presence: true,
+                            length: { minimum: 8, maximum: 32 }
+
+  validates :terms_of_use,  acceptance: true
+
   accepts_nested_attributes_for :profile
-  validates :email, :password, :role, presence: true
-  validates :email, format: { with: /([\.\w\-]+@\w+\.\w{2,3}(?:\.?\.?\w{2,3})?)/ }
-  validates :password, length: { minimum: 8, maximum: 30 }
-  enum role: [:regular, :editor, :admin]
-  validates :terms_of_use, acceptance: true
 
   def full_name
     [profile.first_name, profile.last_name].join(' ') if profile.present?
