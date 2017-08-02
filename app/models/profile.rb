2f3db84174e2611b27e4e492a34de714de8685eb
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-03 01:18:34 +0300

    Profile modelinde hizalama ve validasyon düzenlemeleri yapıldı, ayrıca modele small_photo, medium_photo, big_photo metodları eklendi ve before_create de isim ve soy isim bilgileri titleize yapılıp email adresinden gravatar çekildi

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -14,8 +14,42 @@
 #
 #  index_profiles_on_user_id  (user_id)
 #
-
 class Profile < ApplicationRecord
+  before_create :set_gravatar
+  before_create :titleize_attributes
+
   belongs_to :user
-  validates :first_name, :last_name, presence: true
+
+  validates :first_name,  presence: true
+  validates :last_name,   presence: true
+
+  def small_photo
+    get_gravatar 160
+  end
+
+  def medium_photo
+    get_gravatar 320
+  end
+
+  def big_photo
+    get_gravatar 640
+  end
+
+  private
+
+  def get_gravatar(size)
+    [gravatar, '.png?s=', size].join('')
+  end
+
+  def set_gravatar
+    self.gravatar = [
+      'https://www.gravatar.com/avatar/',
+      Digest::MD5.hexdigest(user.email)
+    ].join('')
+  end
+
+  def titleize_attributes
+    self.first_name = first_name.titleize
+    self.last_name = last_name.titleize
+  end
 end
