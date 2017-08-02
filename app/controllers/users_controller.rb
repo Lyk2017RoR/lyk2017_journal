48c10959ae2b1be2595b73196894aa613a00d962
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-02 17:19:37 +0300

    Users controller da new de redirect yerine render kullanılıyor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), notice: 'Hesabınız başarıyla oluşturuldu'
     else
-      redirect_to new_user_path, notice: "Kaydedilemedi."
+      render 'new'
     end
   end
 
@@ -35,6 +36,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, profile_attributes: [ :first_name, :last_name ])
+    params.require(:user).permit(:email, :password, :password_confirmation, :terms_of_service, profile_attributes: [ :first_name, :last_name ])
   end
 end
