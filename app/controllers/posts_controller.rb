e80d976ddf8c34ef08573d177463d4ea3d255d6c
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-02 10:22:17 +0300

    Postların tamamı yerine statüsü visible olanların index action da listelenmesi sağlandı

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authorize, except: [:index, :show]
   before_action :find_post, only: [:edit, :show, :destroy, :update]
   def index
-    @posts = Post.all
+    @posts = Post.where(status: :visible)
   end
 
   def new
