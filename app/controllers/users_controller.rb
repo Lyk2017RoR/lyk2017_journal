ce7ac5d9eeb163727eb5fa5fd3d78612c6b800c7
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2017-08-02 11:22:13 +0300

    remove byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      byebug
       redirect_to new_user_path, notice: "Kaydedilemedi."
     end
   end
